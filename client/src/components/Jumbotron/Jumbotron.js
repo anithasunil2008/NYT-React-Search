0b2b94ad391f734904d7f081215185a7f49fe6dd
anithasunil2008 <anithasunil2008@gmail.com>
2018-05-06 18:38:12 -0400

    working code

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () =>
+  <div className="jumbotron text-center">
+    <h2>New York Times Article Scrubber</h2>
+    <h3>Search for and save articles of interest.</h3>
+  </div>;
+
+export default Jumbotron;
